8fd1ec6ff9c6d40f43d03ac873c80961954e71f4
Quan Pham <quanpt@gmail.com>
2017-10-14 21:53:42 +1100

    Added distance column to Sydney CBD

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import urllib.request
 import time, base64, subprocess
+import csv
 from lxml import etree
 
 
@@ -8,8 +9,10 @@ def url_retrieve(url, file_name):
     urllib.request.urlretrieve(url, file_name)
 
 
+
+
 # extract data from domain page to csv
-def write_domain_csv(file_name):
+def write_domain_csv(file_name, distanceDict):
     suburb_expr = '//div[@class="suburb-listings" and descendant::h6[@class="suburb-listings__heading"]]'
     suburb_text_expr = './/h6[@class="suburb-listings__heading"]'
     property_link_expr = './/a[@class="auction-details"]'
@@ -18,13 +21,13 @@ def write_domain_csv(file_name):
     property_type_expr = './/span[@class="auction-details__property-type"]'
     price_expr = './/span[@class="auction-details__price"]'
     href_expr = './@href'
-    print_format = '"{}"\t"{}"\t{}\t"{}"\t{}\t"{}"\n'
+    print_format = '"{}"\t"{}"\t{}\t"{}"\t{}\t"{}"\t{}\n'
 
     html_parser = etree.HTMLParser(encoding='utf-8', recover=True,
                                    strip_cdata=True)
     document = etree.parse(file_name, html_parser)
     with open('domain_{}.csv'.format(time.strftime("%Y_%m_%d")), 'w') as output:
-        output.write(print_format.format('Suburb', 'Address', 'Bedroom', 'Type', 'Price', 'Link'))
+        output.write(print_format.format('Suburb', 'Address', 'Bedroom', 'Type', 'Price', 'Link', 'Distance'))
         for e in document.xpath(suburb_expr):
             suburb_name = e.xpath(suburb_text_expr)[0].text
             for property_link in e.xpath(property_link_expr):
@@ -38,7 +41,8 @@ def write_domain_csv(file_name):
                                                  property_link.xpath(bedroom_expr)[0].text,
                                                  property_link.xpath(property_type_expr)[0].text,
                                                  price,
-                                                 property_link.xpath(href_expr)[0]))
+                                                 property_link.xpath(href_expr)[0],
+                                                 distanceDict.get(suburb_name.upper(), 'unknown')))
 
 
 def img2room(href):
@@ -65,7 +69,7 @@ def getXpath(element, xpath, default):
 
 
 # extract data from realestate page to csv
-def write_realestate_csv(file_name):
+def write_realestate_csv(file_name, distanceDict):
     suburb_expr = '//div[@class="rui-table-responsive suburb" and descendant::div[@class="col-suburb-name"]]'
     suburb_text_expr = './/div[@class="col-suburb-name"]'
     property_link_expr = './/tbody/tr'
@@ -74,13 +78,13 @@ def write_realestate_csv(file_name):
     property_type_expr = './/div[@class="col-property-type"]/text()'
     price_expr = './/div[@class="col-property-price"]/text()'
     href_expr = './/a[@class="col-address"]/@href'
-    print_format = '"{}","{}",{},"{}",{},"{}"\n'
+    print_format = '"{}"\t"{}"\t{}\t"{}"\t{}\t"{}"\t{}\n'
 
     html_parser = etree.HTMLParser(encoding='utf-8', recover=True,
                                    strip_cdata=True)
     document = etree.parse(file_name, html_parser)
     with open('realestate_{}.csv'.format(time.strftime("%Y_%m_%d")), 'w') as output:
-        output.write(print_format.format('Suburb', 'Address', 'Bedroom', 'Type', 'Price', 'Link'))
+        output.write(print_format.format('Suburb', 'Address', 'Bedroom', 'Type', 'Price', 'Link', 'Distance'))
         for e in document.xpath(suburb_expr):
             suburb_name = e.xpath(suburb_text_expr)[0].text
             for property_link in e.xpath(property_link_expr):
@@ -93,26 +97,35 @@ def write_realestate_csv(file_name):
                                                  property_link.xpath(address_expr)[0],
                                                  bedroom,
                                                  property_link.xpath(property_type_expr)[0],
-                                                 price, href))
+                                                 price, href, distanceDict.get(suburb_name.upper(), 'un_known')))
 
 
 # fully extract domain result page
-def extract_domain_csv():
+def extract_domain_csv(distanceDict):
     url = 'https://www.domain.com.au/auction-results/sydney/'
     file_name = 'domain.html'
     url_retrieve(url, file_name)
-    write_domain_csv(file_name)
+    write_domain_csv(file_name, distanceDict)
 
 
 # fully extract realestate result page
-def extract_realestate_csv():
+def extract_realestate_csv(distanceDict):
     url = 'https://www.realestate.com.au/auction-results/nsw'
     file_name = 'realestate.html'
     url_retrieve(url, file_name)
-    write_realestate_csv(file_name)
+    write_realestate_csv(file_name, distanceDict)
 
+
+def import_distance(filename):
+    distance = {}
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            distance[row[0].upper()] = row[2]
+    return distance
 
 # run both domain and realestate extraction
 if __name__ == '__main__':
-    extract_domain_csv()
-    extract_realestate_csv()
+    distance = import_distance('distance.csv')
+    extract_domain_csv(distance)
+    extract_realestate_csv(distance)
